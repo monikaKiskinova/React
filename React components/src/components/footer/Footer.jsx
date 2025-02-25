8e81388a2c62578b2839960b9ef1c27a7fd3e92f
Monika Kiskinova <monikova@abv.bg>
2025-02-25 23:21:49 +0200

    Add Footer component (with styles)

---
+++ React components/src/components/footer/Footer.jsx
@@ -0,0 +1,17 @@
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <>
+            <footer>
+                <div className="footer-top-section">
+                    <h3>Explore the World of Cars</h3>
+                    <p>Discover stunning car galleries, detailed insights, and the beauty of automotive design—all in one place.</p>
+                </div>
+                <div className="all-rights-reserved">
+                    <p className="all-rights-reserved">&copy; 2025 - Car Gallery. All rights reserved.</p>
+                </div>
+            </footer>
+        </>
+    )
+}
